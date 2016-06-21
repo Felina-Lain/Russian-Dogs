82661eb709bb7219c259c9a743b17a9e504f0c43
Felina-Lain <sawula@free.fr>
2016-06-21 10:24:15 +0200

    camera avec cadrage if wanted + prefabs et materials + scent system

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Manager : MonoBehaviour {
+
+	public static List<Sprite> s_iconsL = new List<Sprite>();
+	public List<Sprite> _iconsL = new List<Sprite>();
+
+	public static List<string> s_scentL= new List<string>();
+	public List<string> _scentL = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+
+		s_iconsL = _iconsL;
+		s_scentL = _scentL;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/ScentInfo.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ScentInfo : MonoBehaviour {
+
+	public GameObject _icon;
+
+
+	void OnTriggerStay (Collider other) {
+
+		if (other.tag == "Player") {
+			
+			for (int i = 0; i < Manager.s_scentL.Count; i++) {
+
+
+				if (Manager.s_scentL [i] == this.name) {
+					_icon.GetComponent<Image> ().enabled = true;
+					_icon.GetComponent<Image> ().overrideSprite = Manager.s_iconsL[i] as Sprite;
+				}
+
+			}
+					
+		}
+	
+	}
+
+	void OnTriggerExit (Collider other){
+	
+		if (other.tag == "Player") {
+		
+			_icon.GetComponent<Image> ().enabled = false;
+		
+		}
+	
+	}
+}
