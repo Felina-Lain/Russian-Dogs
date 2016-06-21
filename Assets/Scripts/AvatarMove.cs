90ba81a842b7002bbfedebe740d19a7a5898e0ff
Felina-Lain <sawula@free.fr>
2016-06-21 09:42:55 +0200

    1rst push, controller

---
+++ Assets/Scripts/AvatarMove.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarMove : MonoBehaviour {
+
+	public float Speed = 0f;
+	public float MaxJumpTime = 2f;
+	public float JumpForce;
+	private float move = 0f;
+	private float JumpTime = 0f;
+	private bool CanJump;
+
+
+	void Start () {
+		JumpTime  = MaxJumpTime;
+	}
+
+
+	void Update ()
+	{
+		if (!CanJump)
+			JumpTime  -= Time.deltaTime;
+		if (JumpTime <= 0)
+		{
+			CanJump = true;
+			JumpTime  = MaxJumpTime;
+		}
+	}
+
+	void FixedUpdate () {
+		move = Input.GetAxis ("Horizontal");
+		GetComponent<Rigidbody>().velocity = new Vector3 (move * Speed, GetComponent<Rigidbody>().velocity.y);
+		if (Input.GetKey (KeyCode.UpArrow)  && CanJump)
+		{
+			GetComponent<Rigidbody>().AddForce (new Vector3 (GetComponent<Rigidbody>().velocity.x,JumpForce));
+			CanJump = false;
+			JumpTime  = MaxJumpTime;
+		}
+	}
+}
