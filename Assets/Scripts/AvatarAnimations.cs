4445a18e322cc62ebe7e118d4f5afbb7a9b19647
VPlouzeau <viviane.plouzeau@gmail.com>
2016-06-21 13:40:43 +0200

    Modified and expanded camera + player settings + started adding animations.

---
+++ Assets/Scripts/AvatarAnimations.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AvatarAnimations : MonoBehaviour 
+{
+	public GameObject PlayerSprite;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		SpriteRenderer playersr = PlayerSprite.GetComponent<SpriteRenderer> ();
+
+		if (Manager.player_direction == "left") 
+		{
+
+			playersr.flipX = true;
+
+		} 
+		else 
+		{
+			playersr.flipX = false;
+		}
+	
+	}
+}
--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -11,6 +11,12 @@ public class Manager : MonoBehaviour {
 	public static List<string> s_scentL= new List<string>();
 	public List<string> _scentL = new List<string>();
 
+	//reads the direction the player is facing and moving towards. Either "left" or "right".
+	public static string player_direction;
+	public static int player_platformlevel;
+
+	public GameObject player;
+
 	// Use this for initialization
 	void Start () {
 
@@ -21,6 +27,39 @@ public class Manager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		//Reading input direction to determine various data (camera position, player sprite, etc)
+		if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+		{
+			float dir = Input.GetAxis ("Horizontal");
+			if(dir <= 0)
+			{
+				player_direction = "left";
+			}
+			else
+			{
+				player_direction = "right";
+			}
+		}
+
+		//reading the platform level on which the player is
+
+		if (player.transform.position.y < 0) 
+		{
+			player_platformlevel = 1;
+		}
+
+		if (player.transform.position.y >= 0 && player.transform.position.y < 3) 
+		{
+			player_platformlevel = 2;
+		}
+
+		if (player.transform.position.y >= 3) 
+		{
+			player_platformlevel = 3;
+		}
+
+		Debug.Log (player_platformlevel);
 	
 	}
 }
