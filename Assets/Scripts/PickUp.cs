eb3ba978588c3e4cd91de47ac3baddc11a0dbb76
Felina-Lain <sawula@free.fr>
2016-06-21 14:33:31 +0200

    added begining of pick and noms

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+
+	public bool _holding;
+
+
+	
+	// Update is called once per frame
+	void OnTriggerStay (Collider other) {
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+		
+			_holding =! _holding;
+		
+		}
+		GameObject _pick = other.gameObject;
+
+		if (_pick.GetComponent<ObjectClass> ()._pickable && _holding) {
+		
+			_pick.transform.parent = this.transform;
+		
+		} else {
+
+			_pick.transform.parent = null;
+			_pick = null;
+		
+		}
+	
+	}
+		
+}
