af1ec40196532a36f700733a73ccbd7ab84f98e7
VPlouzeau <viviane.plouzeau@gmail.com>
2016-06-21 17:21:52 +0200

    Added smell collision spheres (wip).

--- Assets/Scripts/ScentDetect.cs
+++ Assets/Scripts/ScentDetect.cs
@@ -3,13 +3,33 @@ using System.Collections;
 
 public class ScentDetect : MonoBehaviour {
 
+	public Collider2D selfcollider;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		selfcollider = this.GetComponentInChildren<Collider2D>();
+
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		//detecting overlap between object smell collider and player smell collider
+		GameObject player = GameObject.FindWithTag ("Player");
+		Vector2 playerPos;
+		playerPos.x = player.transform.position.x;
+		playerPos.y = player.transform.position.y;
+
+		if (selfcollider.OverlapPoint (playerPos)) 
+			{
+			Debug.Log ("Smelling " + this.name);
+		} 
+		else 
+		{
+			Debug.Log ("Not smelling " + this.name);
+		}
+
 	}
 }
