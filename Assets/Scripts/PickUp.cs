3ef2f698df54b519cac1dd9f0fd60e8147a4382b
Felina-Lain <sawula@free.fr>
2016-06-21 15:15:41 +0200

    pickup and eating works keys: r and e

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -5,29 +5,51 @@ public class PickUp : MonoBehaviour {
 
 	public bool _holding;
 
+	public GameObject _pick;
+
 
 	
 	// Update is called once per frame
 	void OnTriggerStay (Collider other) {
 
+		if (other.tag == "item") {
+			_pick = other.gameObject;
+		}
+
 		if (Input.GetKeyDown (KeyCode.R)) {
 		
-			_holding =! _holding;
+			_holding = !_holding;
 		
 		}
-		GameObject _pick = other.gameObject;
-
-		if (_pick.GetComponent<ObjectClass> ()._pickable && _holding) {
-		
-			_pick.transform.parent = this.transform;
-		
-		} else {
-
-			_pick.transform.parent = null;
-			_pick = null;
-		
-		}
-	
 	}
-		
+
+		void  Update () {
+
+		if (_pick == null) {
+			return;
+		}
+
+			if (_pick.GetComponent<ObjectClass> ()._pickable && _holding) {
+
+				_pick.transform.parent = this.transform;
+				_pick.GetComponent<MeshRenderer> ().enabled = false;
+
+				if (Input.GetKey (KeyCode.E) && _pick.GetComponent<ObjectClass> ()._edible) {
+				
+					GetComponent<ObjectClass> ()._health += _pick.GetComponent<ObjectClass> ()._health;
+					Destroy (_pick);
+					_pick = null;
+					
+
+					}
+
+			} else {
+
+				_pick.transform.parent = null;
+				_pick.GetComponent<MeshRenderer> ().enabled = true;
+				_pick = null;
+
+			}
+
+	}
 }
