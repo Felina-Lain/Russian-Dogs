eb053b63c2a2bd447d645063457045ee14455145
Felina-Lain <sawula@free.fr>
2016-06-22 16:25:15 +0200

    Barre de faim

--- Assets/Scripts/Doggy related/Hunger.cs
+++ Assets/Scripts/Doggy related/Hunger.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Hunger : MonoBehaviour {
 
 	public float _start_chrono;
 	public float _chrono;
+	public Image _icon;
+	public Color _color;
+
+	private float _starthealth;
 
 	// Use this for initialization
 	void Start () {
 
 		_chrono = _start_chrono;
+		_starthealth = GetComponent<ObjectClass> ()._health;
 	
 	}
 	
@@ -28,6 +34,8 @@ public class Hunger : MonoBehaviour {
 			Destroy (this.gameObject);
 		
 		}
+		_color = new Color (1, 0, 0, ((25.5f * (_starthealth - GetComponent<ObjectClass> ()._health))/255));
+		_icon.material.color = _color;
 	
 	}
 }
