0e645265d6e41ee75c0e8eba92277183b208d262
VPlouzeau <viviane.plouzeau@gmail.com>
2016-06-23 16:32:48 +0200

    Added scene transition, level cleanup.

--- Assets/Scripts/game base/Killzone.cs
+++ Assets/Scripts/game base/Killzone.cs
@@ -6,7 +6,7 @@ public class Killzone : MonoBehaviour {
 	void OnTriggerEnter (Collider other)
 	{
 		
-		if (other.CompareTag ("Enemy")) 
+		if (other.CompareTag ("Enemy") || other.CompareTag("Pigeon")) 
 		{
 			Destroy (other.gameObject);
 		}
---
+++ Assets/Scripts/game base/SceneTransitions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour 
+{
+	public string Scene;
+	void Update()
+	{
+
+	}
+	void OnTriggerEnter (Collider other)
+	{
+
+		if (other.CompareTag ("Player")) 
+		{
+			Debug.Log ("Player here");
+			SceneManager.LoadScene(Scene);
+		}
+	}
+
+}
